6c4f031a10ce237b75f055f9a00b597ccf08f438
Gabriel Phua <gabrielphua@gmail.com>
2016-10-16 07:19:46 +0000

    Got conversion working.

--- Assets/JS/update.js
+++ Assets/JS/update.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-    $("#leftbox").keyup(function(){
+$(document).ready(function() {
+    $("#leftbox").keyup(function() {
         // Getting the current value of textarea
         var currentText = $(this).val();
         
-        var inputCurrency = document.getElementById('inputCurrency');
+        var inputCurrency = document.getElementById('#inputCurrency');
         var inputText = inputCurrency.textContent;
         var inputLowerCase = inputText.toLowerCase();
         
         var outputCurrency = document.getElementById('outputCurrency');
-        var outputText = outputCurrency.textContent
+        var outputText = outputCurrency.textContent;
         var outputLowerCase = outputText.toLowerCase();
         
-        var exchangerate = $.get('/api/' + inputLowerCase + '/' + outputLowerCase + '.php');
-        
-        // Setting the Div content
-        var outputbox = document.getElementById("rightbox");
-				outputbox.value = currentText * exchangerate;
+        $.get('/api/' + inputLowerCase + '/' + outputLowerCase + '.php', function(data) {
+            output = currentText * data;
+    		$("#rightbox").val(output);
+        });
     });
 });
+
